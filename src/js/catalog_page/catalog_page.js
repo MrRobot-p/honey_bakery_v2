e51815e8b54d2170f484ee0cca1920cf0f88ac40
Alexey <73814427+MrRobot-p@users.noreply.github.com>
2022-03-22 23:37:36 +0300

    Update catalog_page.js
    Нет доступа к базе Atlas

--- src/js/catalog_page/catalog_page.js
+++ src/js/catalog_page/catalog_page.js
@@ -1,17 +1,17 @@
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 
-const client = new MongoClient('mongodb+srv://PhantomAD:5486784599@honeybakery.hb0lq.mongodb.net/HoneyBakery?retryWrites=true&w=majority');
+// const client = new MongoClient('mongodb+srv://PhantomAD:5486784599@honeybakery.hb0lq.mongodb.net/HoneyBakery?retryWrites=true&w=majority');
 
-const start = async() => {
-    try {
-        await client.connect()
-        console.log('Соединение установлено')
-        const users = client.db().collection('users')
-        const user = await users.findOne({ name: "Александра" })
-        console.log(user)
-    } catch (e) {
-        console.log(e)
-    }
-}
+// const start = async() => {
+//     try {
+//         await client.connect()
+//         console.log('Соединение установлено')
+//         const users = client.db().collection('users')
+//         const user = await users.findOne({ name: "Александра" })
+//         console.log(user)
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
 
-start();
+// start();
